b43c8e4558ac0a265c391f6a04f8b9466ba9321b
Arkhipenka Andrei <europeoplebelarus@gmail.com>
2020-02-24 19:18:42 +0300

    featL implemented function min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == 0 || array == undefined){
+  	return 0;
+  }
+  let result = array.sort(function(a,b){
+  	return a - b;
+  })
+  return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(array == 0 || array == undefined){
+	 return 0;
+	}
+	let result = array.sort(function(a,b){
+	 return b - a;
+	})
+  	return result[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(array == 0 || array == undefined){
+		return 0;
+	}
+
+	let result = 0;
+	for(let i = 0; i < array.length; i++ ){
+		result += array[i];
+	}
+  return result / array.length;
 }
